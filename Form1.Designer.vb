9d29b5cf976a4f3d51ec640a004d0991dda97e9b
leonel Sittner <leonelsittnerp20@gmail.com>
2023-06-24 11:50:28 -0300

    la-mitad

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,28 +23,52 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.dtvMatriz = New System.Windows.Forms.DataGridView()
+        Me.lblVector = New System.Windows.Forms.ListBox()
+        CType(Me.dtvMatriz, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(632, 81)
+        Me.Button1.Location = New System.Drawing.Point(678, 50)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
         Me.Button1.Text = "Ejecutar"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'dtvMatriz
+        '
+        Me.dtvMatriz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dtvMatriz.Location = New System.Drawing.Point(30, 31)
+        Me.dtvMatriz.Name = "dtvMatriz"
+        Me.dtvMatriz.Size = New System.Drawing.Size(604, 223)
+        Me.dtvMatriz.TabIndex = 1
+        '
+        'lblVector
+        '
+        Me.lblVector.FormattingEnabled = True
+        Me.lblVector.Location = New System.Drawing.Point(640, 105)
+        Me.lblVector.Name = "lblVector"
+        Me.lblVector.Size = New System.Drawing.Size(51, 316)
+        Me.lblVector.TabIndex = 2
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblVector)
+        Me.Controls.Add(Me.dtvMatriz)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.dtvMatriz, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents dtvMatriz As DataGridView
+    Friend WithEvents lblVector As ListBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,24 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Matriz(10, 8), vector(10), Menor, F, C, M As Integer
+        For F = 1 To 10
+            For C = 1 To 8
+                Matriz(F, C) = Rnd() * 100
+            Next
+        Next
+        For F = 1 To 8
+            dtvMatriz.Columns.Add("", "")
+        Next
+        dtvMatriz.Rows.Add(9)
+        For F = 1 To 10
+            Menor = Matriz(F, 1)
+            M = M + 1
+            For C = 1 To 8
+                If Matriz(F, C) <= Menor Then
+                    Menor = Matriz(F, C)
+                    vector(M) = Matriz(F, C)
+                End If
+            Next
+        Next
+    End Sub
 End Class
